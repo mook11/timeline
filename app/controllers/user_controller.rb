ad6b40763713b2b40c27a436182f7a147e11220b
sangmook <anrdl147@gmail.com>
2017-01-07 12:47:36 +0000

    회원가입 정보 저장 화면 전환기능 추가

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,22 @@
 class UserController < ApplicationController
   def signup
   end
+
+  def signup_complete
+  u = User.new
+  u.username = params[:username]
+  if params[:password] == params[:retype_password] 
+  	u.password = params[:password]
+	if u.save
+	flash[:alert]="회원 가입 완료"
+	redirect_to "/wall/posts"
+	else
+	flash[:alert]=u.errors.values.flatten.join(' ')
+	redirect_to :back
+	end
+	else
+	flash[:alert] = "패스워드가 다릅니다."
+	redirect_to :back
+	end
+  end
 end
