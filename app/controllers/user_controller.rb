6cc717af025d808f11b9c1782cc54c76cbaac1b9
sangmook <anrdl147@gmail.com>
2017-01-08 06:20:08 +0000

    로그인 화면 만들기

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,17 +6,21 @@ class UserController < ApplicationController
   u = User.new
   u.username = params[:username]
   if params[:password] == params[:retype_password] 
-  	u.password = params[:password]
-	if u.save
-	flash[:alert]="회원 가입 완료"
-	redirect_to "/wall/posts"
+  	u.passward = params[:password]
+	if (u.save)
+		flash[:alert]="회원 가입 완료"
+		redirect_to "/wall/posts"
 	else
-	flash[:alert]=u.errors.values.flatten.join(' ')
-	redirect_to :back
+		flash[:alert]=u.errors.values.flatten.join(' ')
+		redirect_to :back
 	end
-	else
+  else
 	flash[:alert] = "패스워드가 다릅니다."
 	redirect_to :back
-	end
   end
+  end
+
+  def login
+  end
+
 end
