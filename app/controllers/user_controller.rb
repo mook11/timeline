d89f7c880bc29c623daa744cd7a6786e9aeaed79
sangmook <anrdl147@gmail.com>
2017-01-08 06:27:55 +0000

    로그인 기능 추가

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -21,6 +21,23 @@ class UserController < ApplicationController
   end
 
   def login
+  
   end
 
+  def login_complete
+  u = User.where(username: params[:username])[0]
+  if u.nil?
+  	flash[:alert] = "잘못된 아이디거나 패스워드 입니다"
+	redirect_to :back
+  else
+	if u.username != u.passward
+	flash[:alert] = " 잘못된 아이디거나 패스워드 입니다."
+	redirect_to :back
+	else
+	cookies[:user_id] = u.id
+	flash[:alert] = "로그인 성공"
+	redirect_to "/wall/posts"
+	end
+  end
+  end
 end
